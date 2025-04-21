55c946ba1e22d190375dfaab00be764ac294bb56
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-21 16:35:08 -0300

    First commit

---
+++ src/main/java/com/github/devmribeiro/spring/RestApiTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.devmribeiro.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/devmribeiro/rest_api_transactions/RestApiTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devmribeiro.rest_api_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
