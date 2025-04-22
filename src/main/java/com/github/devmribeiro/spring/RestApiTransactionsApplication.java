1c0a9a3d7473a6e3acbbdb5c2de6fdc855434cfe
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-22 02:58:38 -0300

    refactor: remove empty lines

--- src/main/java/com/github/devmribeiro/spring/RestApiTransactionsApplication.java
+++ src/main/java/com/github/devmribeiro/spring/RestApiTransactionsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApiTransactionsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiTransactionsApplication.class, args);
 	}
-
 }
